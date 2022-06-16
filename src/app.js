da06376f43aa85a83e8c61a9ed8a255eaf0a0feb
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-16 12:40:06 +0200

    Been trying to figure out to get the apiKey working but it is not...

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 
 function displayTemperature(response){
-    console.log(resonse.data.main.temp  );
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+
 }
 let apiKey = "cba4b90df0ca1f578fc4dddc5551b537";
 let apiUrl = ´https://api.openweathermap.org/data/2.5/weather?q=${city name}&appid=${apiKey}&units=metric´;
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then((response) => {
+  console.log(response);
+});
